196a93ea8f3b4d2bb37e2c5c3c1d3112c7262007
Качановський Владислав bc2025-2 <kacanovskijvlad@gmail.com>
2026-05-10 22:37:07 +0300

    feat: implement header component with logo and navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Header from "./components/header/Header";
+
 function App() {
   return (
     <div>
-      <h1>Railway Booking System</h1>
+      <Header/>
     </div>
   );
 }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,36 @@
+import styles from './Header.module.css';
+
+function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <a href="/" className={styles.brand}>
+          <span className={styles.logoBox}>
+            <img
+              src="/assets/Logo.svg"
+              alt="Railway Booking Logo"
+              className={styles.logo}
+            />
+          </span>
+
+          <div className={styles.brandText}>
+            <span className={styles.title}>Railway Booking</span>
+            <span className={styles.subtitle}>Пошук потягів та рейсів</span>
+          </div>
+        </a>
+
+        <nav className={styles.nav}>
+          <a href="#routes" className={styles.link}>
+            Рейси
+          </a>
+
+          <a href="#routes" className={styles.button}>
+            Переглянути рейси
+          </a>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
