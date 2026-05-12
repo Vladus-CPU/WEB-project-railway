4f455334db25149faa69e4537d7044bfc84bcbc2
Качановський Владислав bc2025-2 <kacanovskijvlad@gmail.com>
2026-05-12 20:04:12 +0300

    feat: add wagon details to train data

--- server/data/trains.js
+++ server/data/trains.js
@@ -9,7 +9,36 @@ const trains = [
     arrivalTime: '06:20',
     duration: '9 год 20 хв',
     trainType: 'Нічний експрес',
-    price: 540
+    price: 540,
+    wagons: [
+      {
+        id: 1,
+        number: 1,
+        type: 'СВ',
+        classCode: 'Л',
+        seatCount: 18,
+        layout: 'lux',
+        description: 'Двомісні купе підвищеного комфорту'
+      },
+      {
+        id: 2,
+        number: 2,
+        type: 'Купе',
+        classCode: 'К',
+        seatCount: 36,
+        layout: 'coupe',
+        description: 'Чотиримісні закриті купе'
+      },
+      {
+        id: 3,
+        number: 3,
+        type: 'Плацкарт',
+        classCode: 'П',
+        seatCount: 54,
+        layout: 'platzkart',
+        description: 'Відкритий вагон з боковими місцями'
+      }
+    ]
   },
   {
     id: 2,
@@ -21,7 +50,36 @@ const trains = [
     arrivalTime: '13:50',
     duration: '7 год 05 хв',
     trainType: 'Інтерсіті+',
-    price: 690
+    price: 690,
+    wagons: [
+      {
+        id: 1,
+        number: 1,
+        type: 'Сидячий 1 клас',
+        classCode: 'С1',
+        seatCount: 48,
+        layout: 'seatingFirst',
+        description: 'Підвищений комфорт та більше простору'
+      },
+      {
+        id: 2,
+        number: 2,
+        type: 'Сидячий 2 клас',
+        classCode: 'С2',
+        seatCount: 80,
+        layout: 'seatingSecond',
+        description: 'Стандартні сидячі місця'
+      },
+      {
+        id: 3,
+        number: 3,
+        type: 'Сидячий 2 клас',
+        classCode: 'С2',
+        seatCount: 80,
+        layout: 'seatingSecond',
+        description: 'Стандартні сидячі місця'
+      }
+    ]
   },
   {
     id: 3,
@@ -33,7 +91,36 @@ const trains = [
     arrivalTime: '08:10',
     duration: '13 год 40 хв',
     trainType: 'Нічний швидкий',
-    price: 620
+    price: 620,
+    wagons: [
+      {
+        id: 1,
+        number: 1,
+        type: 'СВ',
+        classCode: 'Л',
+        seatCount: 18,
+        layout: 'lux',
+        description: 'Двомісні купе підвищеного комфорту'
+      },
+      {
+        id: 2,
+        number: 2,
+        type: 'Купе',
+        classCode: 'К',
+        seatCount: 36,
+        layout: 'coupe',
+        description: 'Чотиримісні закриті купе'
+      },
+      {
+        id: 3,
+        number: 3,
+        type: 'Плацкарт',
+        classCode: 'П',
+        seatCount: 54,
+        layout: 'platzkart',
+        description: 'Відкритий вагон з боковими місцями'
+      }
+    ]
   },
   {
     id: 4,
@@ -45,7 +132,36 @@ const trains = [
     arrivalTime: '14:15',
     duration: '8 год 30 хв',
     trainType: 'Міжнародний',
-    price: 980
+    price: 980,
+    wagons: [
+      {
+        id: 1,
+        number: 1,
+        type: 'RIC',
+        classCode: 'RIC',
+        seatCount: 30,
+        layout: 'ric',
+        description: 'Міжнародний вагон з трансформованими купе'
+      },
+      {
+        id: 2,
+        number: 2,
+        type: 'Купе',
+        classCode: 'К',
+        seatCount: 36,
+        layout: 'coupe',
+        description: 'Чотиримісні закриті купе'
+      },
+      {
+        id: 3,
+        number: 3,
+        type: 'СВ',
+        classCode: 'Л',
+        seatCount: 18,
+        layout: 'lux',
+        description: 'Двомісні купе підвищеного комфорту'
+      }
+    ]
   },
   {
     id: 5,
@@ -57,7 +173,36 @@ const trains = [
     arrivalTime: '10:35',
     duration: '18 год 25 хв',
     trainType: 'Пасажирський',
-    price: 470
+    price: 470,
+    wagons: [
+      {
+        id: 1,
+        number: 1,
+        type: 'Купе',
+        classCode: 'К',
+        seatCount: 36,
+        layout: 'coupe',
+        description: 'Чотиримісні закриті купе'
+      },
+      {
+        id: 2,
+        number: 2,
+        type: 'Плацкарт',
+        classCode: 'П',
+        seatCount: 54,
+        layout: 'platzkart',
+        description: 'Відкритий вагон з боковими місцями'
+      },
+      {
+        id: 3,
+        number: 3,
+        type: 'Загальний',
+        classCode: 'З',
+        seatCount: 81,
+        layout: 'general',
+        description: 'Економний вагон із сидячим розміщенням'
+      }
+    ]
   }
 ];
 
